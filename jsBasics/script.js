c3a100b12e29e2a41f260125f2ec09bb3c900308
Luciano E. Monteiro <luciano.es.mo@gmail.com>
2022-01-13 00:03:10 -0300

    JS Basics

---
+++ jsBasics/script.js
@@ -0,0 +1,30 @@
+const lista1 = [1, 2, "Pedrinho", "Miguel"];
+const arrayToPush = [5, 2, "Hello"]
+lista1.push(arrayToPush);
+
+// push(valor) - coloca "valor" no final
+// pop() - tira do final
+// unshift(valor) - coloca valor no começo
+// shift() - tira do começo
+
+// for (let i = 0; i <= 10; i = i + 2) {
+//     console.log(i)
+// }
+
+const chave = "Miguel";
+
+switch (chave) {
+    case "Pedro":
+        console.log("Era Pedro mesmo");
+        break;
+    case "Miguel":
+        console.log("Era Miguel.");
+        break;
+    case "Luciano":
+        console.log("Era Luciano");
+        break;
+    default:
+        break;
+}
+
+// console.log();
